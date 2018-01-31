27725b9946cd850047250a8dc4aba00b03fafc4d
Jarrett <Jarrett@DESKTOP-O0B1LES>
2018-01-30 21:25:48 -0600

    First Push

---
+++ src/org/usfirst/frc/team2977/robot/subsystems/PixyException.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team2977.robot.subsystems;
+
+public class PixyException extends Exception{
+	public PixyException(String message){
+		super(message);
+	}
+}
+
---
+++ src/org/usfirst/frc/team2977/robot/subsystems/PixyPacket.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team2977.robot.subsystems;
+
+public class PixyPacket {
+	public int Signature;
+	public int X;
+	public int Y;
+	public int Width;
+	public int Height;
+	
+	//public int checksumError;
+	
+	public String toString() {
+		return "" +
+	" S:" + Signature +
+	" X:" + X + 
+	" Y:" + Y +
+	" W:" + Width + 
+	" H:" + Height;
+	}
+}
+
